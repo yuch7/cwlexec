e3c19121ac9ec8db24f09c542931345a43bb4ef0
liuwei <liuweixa@cn.ibm.com>
2018-06-25 09:51:40 +0800

    Update licence information

--- src/main/java/com/ibm/spectrumcomputing/cwl/parser/BaseParser.java
+++ src/main/java/com/ibm/spectrumcomputing/cwl/parser/BaseParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright International Business Machines Corp, 2018.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

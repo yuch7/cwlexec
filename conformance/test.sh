fde77a5892e40f8a3a096c2b72a863c723e7d5d8
kittjx <kittjx@hotmail.com>
2019-11-05 15:20:57 +0800

    init

--- conformance/test.sh
+++ conformance/test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pwd
-ls
+
+groupadd docker
+sudo gpasswd -a lsfadmin docker
+source /opt/ibm/lsfsuite/lsf/conf/profile.lsf
+source <(head -n265 /start_lsf_ce.sh | tail -n +7)
+ROLE=master config_lsfce
+ROLE=master start_lsf
+lsid
+lshosts
+
+cd /cwlexec/src/test/integration-test
+

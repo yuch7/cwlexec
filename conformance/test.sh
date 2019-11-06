9a13a0c5cdf35368498f6c9f4bbc8f58c8893b2a
kittjx <kittjx@hotmail.com>
2019-11-06 13:51:01 +0800

    init

--- conformance/test.sh
+++ conformance/test.sh
@@ -2,7 +2,14 @@
 
 groupadd docker
 gpasswd -a lsfadmin docker
-source /opt/ibm/lsfsuite/lsf/conf/profile.lsf
+
+LSF_TOP=/opt/ibm/lsfsuite/lsf
+echo 'LSB_RESOURCE_ENFORCE="cpu memory"' >> $LSF_TOP/conf/lsf.conf
+IMAGE_HOST=`awk -F'"' '/MASTER_LIST/ {print $(NF-1)}' $LSF_TOP/conf/lsf.conf`
+sed -i "s/${IMAGE_HOST}.*$/${IMAGE_HOST}   \!   \!   1   \(mg docker\)/g" $LSF_TOP/conf/lsf.cluster.cluster1
+sed -i "/End Resource/i\   docker     Boolean \(\)       \(\)          \(docker\)" $LSF_TOP/conf/lsf.shared
+
+source $LSF_TOP/conf/profile.lsf
 source <(head -n265 /start_lsf_ce.sh | tail -n +7)
 ROLE=master config_lsfce
 ROLE=master start_lsf

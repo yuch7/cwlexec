6a76ddaa44e700f5aebe49cdc9f4a237727e4b79
kittjx <kittjx@hotmail.com>
2019-11-07 14:32:17 +0800

    init

--- conformance/test.sh
+++ conformance/test.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-LSF_TOP=/opt/ibm/lsf
-echo 'LSB_RESOURCE_ENFORCE="cpu memory"' | sudo tee -a $LSF_TOP/conf/lsf.conf
+LSF_TOP=/opt/ibm/lsfsuite/lsf
+echo 'LSB_RESOURCE_ENFORCE="cpu memory"' >> $LSF_TOP/conf/lsf.conf
 IMAGE_HOST=`awk -F'"' '/MASTER_LIST/ {print $(NF-1)}' $LSF_TOP/conf/lsf.conf`
-sed "s/${IMAGE_HOST}.*$/${IMAGE_HOST}   \!   \!   1   \(mg docker\)/g" $LSF_TOP/conf/lsf.cluster.cluster1 > /tmp/tmpfile1
-cat /tmp/tmpfile1 | sudo tee $LSF_TOP/conf/lsf.cluster.cluster1
-sed "/End Resource/i\   docker     Boolean \(\)       \(\)          \(docker\)" $LSF_TOP/conf/lsf.shared > /tmp/tmpfile2
-cat /tmp/tmpfile2 | sudo tee $LSF_TOP/conf/lsf.shared
+sed -i "s/${IMAGE_HOST}.*$/${IMAGE_HOST}   \!   \!   1   \(mg docker\)/g" $LSF_TOP/conf/lsf.cluster.cluster1
+sed -i "/End Resource/i\   docker     Boolean \(\)       \(\)          \(docker\)" $LSF_TOP/conf/lsf.shared
 
 source $LSF_TOP/conf/profile.lsf
-source <(head -n265 /opt/ibm/start_lsf_ce.sh | tail -n +7)
-ROLE=master sudo config_lsfce
-ROLE=master sudo start_lsf
-lsid
-lshosts
+source <(head -n265 /start_lsf_ce.sh | tail -n +7)
+ROLE=master config_lsfce
+#ROLE=master sudo start_lsf
 
+cp -rf $LSF_TOP /hostibm
 #su lsfadmin -c "source /opt/ibm/lsf/conf/profile.lsf && cd src/test/integration-test && ./run.sh"
 

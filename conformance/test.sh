e1b137eff186a64e8eb30657cded37b37b489750
kittjx <kittjx@hotmail.com>
2019-11-07 12:58:18 +0800

    init

--- conformance/test.sh
+++ conformance/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 LSF_TOP=/opt/ibm/lsf
-echo 'LSB_RESOURCE_ENFORCE="cpu memory"' >> $LSF_TOP/conf/lsf.conf
+sudo echo 'LSB_RESOURCE_ENFORCE="cpu memory"' >> $LSF_TOP/conf/lsf.conf
 IMAGE_HOST=`awk -F'"' '/MASTER_LIST/ {print $(NF-1)}' $LSF_TOP/conf/lsf.conf`
-sed -i "s/${IMAGE_HOST}.*$/${IMAGE_HOST}   \!   \!   1   \(mg docker\)/g" $LSF_TOP/conf/lsf.cluster.cluster1
+sudo sed -i "s/${IMAGE_HOST}.*$/${IMAGE_HOST}   \!   \!   1   \(mg docker\)/g" $LSF_TOP/conf/lsf.cluster.cluster1
 sed -i "/End Resource/i\   docker     Boolean \(\)       \(\)          \(docker\)" $LSF_TOP/conf/lsf.shared
 
 source $LSF_TOP/conf/profile.lsf
 source <(head -n265 $LSF_TOP/start_lsf_ce.sh | tail -n +7)
-ROLE=master config_lsfce
-ROLE=master start_lsf
+ROLE=master sudo config_lsfce
+ROLE=master sudo start_lsf
 lsid
 lshosts
 
